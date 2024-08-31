0abbf2c66ff2dc7067664f037b235bdeeeb9fa7f
StarKnightt <prasenjitt4e@gmail.com>
2024-08-31 14:54:48 +0530

    fixing the dependencies

--- src/components/MemeGenerator.jsx
+++ src/components/MemeGenerator.jsx
@@ -40,17 +40,17 @@ const MemeGenerator = () => {
 
   return (
     <motion.div
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5 }}
-    className="flex flex-col items-center justify-center p-6 text-gray-800"
-  >
-    <AnimatedText
-      text="GitHub Reacts"
-      className="text-6xl font-bold mb-10 text-center"
-    />
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="flex flex-col items-center justify-center p-6 text-gray-800"
+    >
+      <AnimatedText
+        text="GitHub Reacts"
+        className="text-6xl font-bold mb-10 text-center"
+      />
       <motion.div
-        className="bg-white bg-opacity-40 p-10 rounded-lg shadow-2xl w-full max-w-lg backdrop-filter backdrop-blur-sm"
+        className="bg-white bg-opacity-60 p-10 rounded-lg shadow-2xl w-full max-w-lg backdrop-filter backdrop-blur-sm"
         whileHover={{ scale: 1.02 }}
         transition={{ type: "spring", stiffness: 300 }}
       >
@@ -72,6 +72,7 @@ const MemeGenerator = () => {
             Insult
           </Button>
         </div>
+
         <AnimatePresence>
           {error && (
             <motion.p
@@ -120,17 +121,27 @@ const MemeGenerator = () => {
   );
 };
 
-const Button = ({ onClick, disabled, color, icon, children }) => (
-  <motion.button
-    onClick={onClick}
-    disabled={disabled}
-    className={`flex items-center justify-center px-6 py-3 text-white bg-${color}-500 rounded-md hover:bg-${color}-600 focus:outline-none focus:ring-2 focus:ring-${color}-500 disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-300 w-full text-lg`}
-    whileHover={{ scale: 1.05 }}
-    whileTap={{ scale: 0.95 }}
-  >
-    {icon && <span className="mr-2">{icon}</span>}
-    {children}
-  </motion.button>
-);
+const Button = ({ onClick, disabled, color, icon, children }) => {
+  const baseClasses = 'flex items-center justify-center px-6 py-3 text-white rounded-md focus:outline-none focus:ring-2 disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-300 w-full text-lg';
+
+  const colorClasses = {
+    green: 'bg-green-500 hover:bg-green-800 focus:ring-green-500',
+    red: 'bg-red-500 hover:bg-red-600 focus:ring-red-500',
+  };
+
+  return (
+    <motion.button
+      onClick={onClick}
+      disabled={disabled}
+      className={`${baseClasses} ${colorClasses[color]}`}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      {icon && <span className="mr-2">{icon}</span>}
+      {children}
+    </motion.button>
+  );
+};
+
 
 export default MemeGenerator;
